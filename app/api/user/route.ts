c5d0b176a7502e6acb1fd36a747337c1c280cdb7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-26 12:00:30 +0300

    Shenanigans

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -5,7 +5,7 @@ export const GET = async () => {
     const users = await prisma.user.findMany({
         where: {
             name: {
-                startsWith: 'J'
+                endsWith: 'n'
             }
         }
     })
