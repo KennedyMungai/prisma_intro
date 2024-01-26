1aa756b48ce1c6a6a43d1926a39504c295a9bd2e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-26 11:56:55 +0300

    Created the route to retrieve all the users from a database

---
+++ app/api/user/route.ts
@@ -0,0 +1,8 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+    const users = await prisma.user.findMany()
+
+    return NextResponse.json(users)
+}
