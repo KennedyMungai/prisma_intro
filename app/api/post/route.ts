c37a028125c977fddd21036b08ca0f39f59a99aa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-26 12:13:18 +0300

    Got a list of all the post where the author's name is Jack

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -2,6 +2,14 @@ import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export const GET = async () => {
-    const posts = await prisma.post.findMany()
+    const posts = await prisma.post.findMany({
+        where: {
+            author: {
+                is: {
+                    name: 'Jack'
+                }
+            }
+        }
+    })
     return NextResponse.json(posts)
 }
