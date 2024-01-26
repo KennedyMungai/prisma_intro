2941cf10d8834aebe1d0bb62e2970999f02c536f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-26 11:58:19 +0300

    Added a filter for the users to be retrieved from the database

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,7 +2,11 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({
+        where: {
+            name: 'sara'
+        }
+    })
 
     return NextResponse.json(users)
 }
