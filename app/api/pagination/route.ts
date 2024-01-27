13f33f675bbc3b956dc8967d21d2c5b4c9db9b7d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-27 10:13:39 +0300

    Implemented some pagination logic to the prisma route

--- app/api/pagination/route.ts
+++ app/api/pagination/route.ts
@@ -1,10 +1,15 @@
 import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-export const GET = async () => {
+export const GET = async (request: Request) => {
+    const { searchParams } = new URL(request.url)
+
+    const pgnum = +(searchParams.get("pgnum") ?? 0)
+    const pgsize = +(searchParams.get("pgsize") ?? 2)
+
     const paginatedPosts = await prisma.post.findMany({
-        skip: 2,
-        take: 2
+        skip: pgnum,
+        take: pgsize
     })
 
     return NextResponse.json(paginatedPosts)
