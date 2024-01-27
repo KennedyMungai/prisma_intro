4a4af9ae91ff368cfaa58b6780cf86abeeb68b76
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-27 11:04:51 +0300

    Changed the api endpoint code so that the multiple users can be created at once

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -2,31 +2,48 @@ import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export const POST = async () => {
-    const user = await prisma.user.create({
-        data: {
-            email: 'some2@body.com',
-            name: 'Some2 Body',
-            role: 'USER',
-            post: {
-                create: [
-                    {
-                        title: 'Some title',
-                        isPublished: true,
-                        categories: {
-                            connectOrCreate: {
-                                where: {
-                                    id: 3
-                                },
-                                create: {
-                                    name: "ORM"
-                                }
-                            }
-                        }
-                    }
-                ]
+    // const user = await prisma.user.create({
+    //     data: {
+    //         email: 'some2@body.com',
+    //         name: 'Some2 Body',
+    //         role: 'USER',
+    //         post: {
+    //             create: [
+    //                 {
+    //                     title: 'Some title',
+    //                     isPublished: true,
+    //                     categories: {
+    //                         connectOrCreate: {
+    //                             where: {
+    //                                 id: 3
+    //                             },
+    //                             create: {
+    //                                 name: "ORM"
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             ]
+    //         }
+    //     }
+    // })
+
+    const users = await prisma.user.createMany({
+        data: [
+            {
+                name: 'Sha Poopie', 
+                email: 'Sha@poopie.com'
+            },
+            { 
+                name: 'Thun Der', 
+                email: 'thun@der.com' 
+            },
+            {
+                name: "Kitchen Sink", 
+                email: "kitchen@sink.com"
             }
-        }
+        ]
     })
 
-    return NextResponse.json(user)
+    return NextResponse.json(users)
 }
