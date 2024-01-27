e6994c01de3c78b111ee2704cd8c2857d265f8b6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-27 11:34:40 +0300

    Updated the route that updates users in the database

--- app/api/user/update/[id]/route.ts
+++ app/api/user/update/[id]/route.ts
@@ -1,12 +1,18 @@
 import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-export const PUT = async ({params}: {params: {id: string}}) => {
-    console.log(params.id)
+type Props = {
+    params: {
+        id: string
+    }
+}
+
+export const PUT = async (request: Request, {params: {id}}: Props) => {
+    console.log(id)
 
     const updatedUser = await prisma.user.update({
         where: {
-            id: Number(params.id)
+            id: Number(id)
         },
         data: {
             name: "Updated User"
